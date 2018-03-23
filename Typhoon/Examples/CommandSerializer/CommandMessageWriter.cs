a56b61b83950443bbaf5ec9c8f1dea8abe364343
D.Snezhkov <dsnezhkov@gmail.com>
2018-03-22 20:19:32 -0500

    control-c trapping

---
+++ Typhoon/Examples/CommandSerializer/CommandMessageWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typhoon
+{
+    public static class CommandMessageWriter
+    {
+
+        public static void MsgWrite()
+        {
+            // First write something so that there is something to read ...  
+            var b = new CommandMessage { title = "Serialization Overview" };
+            var writer = new System.Xml.Serialization.XmlSerializer(typeof(CommandMessage));
+            var wfile = new System.IO.StreamWriter(@"c:\temp\SerializationOverview.xml");
+            writer.Serialize(wfile, b);
+            wfile.Close();
+        }
+    }
+
+
+
+}
